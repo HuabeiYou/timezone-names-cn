bfae88caac6df7f761d6f802962110addae9c8a3
Huabei You <huabeiyou@gmail.com>
2022-11-26 22:30:43 -0500

    add utcOffset function

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const en = require("./cn-en.json");
 const cn = require("./en-cn.json");
 
+function utcOffset(tz) {
+  const timeZone = en[tz] || tz;
+  if (!timeZone) return 0;
+  const current = new Date();
+  current.setMilliseconds(0);
+  const targetTzString = current.toLocaleString("en-GB", { timeZone });
+  if (!targetTzString) return 0;
+  // DD/MM/YYYY, HH:mm:ss
+  const [targetDateString, targetTimeString] = targetTzString.split(",");
+  if (!targetDateString || !targetTimeString) return 0;
+  const [day, month, year] = targetDateString.trim().split("/");
+  const [hour, minite, second] = targetTimeString.trim().split(":");
+  const utc = Date.UTC(
+    Number(year),
+    Number(month) - 1,
+    Number(day),
+    Number(hour),
+    Number(minite),
+    Number(second)
+  );
+  return Math.round((utc - current) / 60000);
+}
+
 module.exports = {
   en,
   cn,
+  utcOffset,
 };
