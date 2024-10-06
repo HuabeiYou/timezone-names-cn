8b4dc43b5db23563880271a627b0e272317731c6
HuabeiYou <huabeiyou@gmail.com>
2024-10-06 14:39:01 +0800

    feat: add utcOffsetText util function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const en = require("./cn-en.json");
 const cn = require("./en-cn.json");
-
-function utcOffset(tz) {
+/**
+ * Calculates the UTC offset in minutes for a given IANA timezone.
+ *
+ * @param {string} tz - IANA timezone identifier (e.g., "America/New_York").
+ * @param {Date} [targetDate=new Date()] - The date for which to calculate the offset. Defaults to the current date and time.
+ * @returns {number} The UTC offset in minutes. Returns 0 if the offset couldn't be determined.
+ */
+function utcOffset(tz, targetDate = new Date()) {
   const timeZone = en[tz] || tz;
   if (!timeZone) return 0;
-  const current = new Date();
+  const current = new Date(targetDate);
   current.setMilliseconds(0);
   const targetTzString = current.toLocaleString("en-GB", { timeZone });
   if (!targetTzString) return 0;
@@ -19,13 +25,33 @@ function utcOffset(tz) {
     Number(day),
     Number(hour),
     Number(minite),
-    Number(second)
+    Number(second),
   );
   return Math.round((utc - current) / 60000);
+}
+
+/**
+ * Retrieves the UTC offset text for a given IANA timezone.
+ *
+ * @param {string} tz - IANA timezone identifier (e.g., "America/New_York").
+ * @param {Date} [targetDate=new Date()] - The date for which to calculate the offset. Defaults to the current date and time.
+ * @returns {string | undefined} The UTC offset text in the format "GMT±HH:MM" or undefined if the offset couldn't be determined.
+ */
+function utcOffsetText(tz, targetDate = new Date()) {
+  const timeZone = en[tz] || tz;
+  const offset = new Intl.DateTimeFormat(undefined, {
+    timeZone,
+    timeZoneName: "shortOffset",
+  })
+    .formatToParts(targetDate)
+    .find((part) => part.type === "timeZoneName")?.value;
+  return offset;
 }
 
 module.exports = {
   en,
   cn,
   utcOffset,
+  utcOffsetMinutes: utcOffset,
+  utcOffsetText,
 };
